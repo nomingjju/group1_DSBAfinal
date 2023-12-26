d19f85d7d2c11f43df12fbf8164966a305f5f1c1
HEEE23 <gmltjs1592@gmail.com>
2023-12-26 16:41:14 +0900

    venvs수정

--- projects/pibu/pibu/main_views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, url_for, render_template
-from pibu.models import SocialLogin
-from werkzeug.utils import redirect
-
-bp = Blueprint('main', __name__, url_prefix='/')
-
-@bp.route('/')
-def index():
-    return redirect(url_for('uvmuhwan._list'))
--- venvs/pibu.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd c:/projects/pibu
-set FLASK_APP=pibu
-set FLASK_DEBUG=true
-c:/venvs/pibu/scripts/activate
